d400f762bf1666c4844a2d95ff36aa82c6197fc7
Scott Reese <scott_reese@pm.me>
2019-07-17 11:13:35 -0400

    removed unnecessary files from repo and added to .gitignore

--- app/routes.py
+++ app/routes.py
@@ -59,4 +59,4 @@ def register():
         db.session.commit()
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+    return render_template('register.html', title='Register', form=form)e
